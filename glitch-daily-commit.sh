5b9e1cf25ea3cb7cec1069e2e3223b6bfbef3491
jsdario <jesusdriru@gmail.com>
2019-01-30 11:50:46 +0000

    Fix config and scripts to reflect the author correctly

--- glitch-daily-commit.sh
+++ glitch-daily-commit.sh
@@ -1,5 +1,5 @@
 
 date > daily-change.txt
 git add daily-change.txt
-git commit -m "Glitch purposeless" --author jsdario
+git commit -m "Glitch purposeless commit for $(date)"
 git push https://github.com/jsdario/daily-github master:glitch
--- glitch-git-global.sh
+++ glitch-git-global.sh
@@ -1,2 +1,2 @@
-  git config --global user.email "jesus.dario@scope.ink"
+  git config --global user.email "jesusdriru@gmail.com"
   git config --global user.name "jsdario"
